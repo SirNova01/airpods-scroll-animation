4c80dbbd645c76eb8064aa4205a417a522a51881
Taye <taye.aduewa@vibes.com>
2024-06-01 15:20:06 +0100

    complete app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Back from "./components/background/Back";
+import NavBar from "./components/navbar/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Back />
+    </div>
+  );
+}
+
+export default App;
